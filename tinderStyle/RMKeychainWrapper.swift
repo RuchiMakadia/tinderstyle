56ff2a35f716e2f1f6f4174838cda45ecc15c6e6
Ruchi Makadia <ruchi.m@eheuristic.com>
2021-02-25 18:00:42 +0530

    add keychain code

---
+++ tinderStyle/RMKeychainWrapper.swift
@@ -0,0 +1,25 @@
+//
+//  RMKeychainWrapper.swift
+//  tinderStyle
+//
+//  Created by eHeuristic on 25/02/21.
+//
+
+import Foundation
+
+/*struct RMKeychainWrapper {
+    static var token: String? {
+        get {
+            return KeychainWrapper.standard.string(forKey: Keys.Keychain.userToken)
+        }
+        set {
+            if let newValue = newValue {
+                KeychainWrapper.standard.set(newValue, forKey: Keys.Keychain.userToken)
+            }
+            else {
+                KeychainWrapper.standard.removeObject(forKey: Keys.Keychain.userToken)
+            }
+        }
+    }
+}*/
+
